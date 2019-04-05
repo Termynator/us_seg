fixed fucking image intensity scaling

4CH_train.py
@@ -9,25 +9,25 @@ import unet
 import params
 
 path = "/home/zeke/Programming/cnn/us_seg/"
-path_2CH = path + "data/2CH_dataset/"
+path_4CH = path + "data/4CH_dataset/"
 model_path = path + "models/"
-numpy_path_2CH = path_2CH + "numpys/"
-image_path_2CH = path_2CH + "image/"
-masks_path_2CH = path_2CH + "masks/"
+numpy_path_4CH = path_4CH + "numpys/"
+image_path_4CH = path_4CH + "image/"
+masks_path_4CH = path_4CH + "masks/"
 
 #load numpys
 
-image_ds = np.load(numpy_path_2CH + "image_nc_ds.npy")
-masks_ds = np.load(numpy_path_2CH + "masks_vent_ds.npy")
+image_ds = np.load(numpy_path_4CH + "image_nc_ds.npy")
+masks_ds = np.load(numpy_path_4CH + "masks_vent_ds.npy")
 
-#for i in range(masks_ds.shape[0]):
+#for i in range(image_ds.shape[0]):
 #    plt.imshow(image_ds[i,:,:,0])
 #    plt.show()
 
 #instantiate params
-name = "2CH_vent"
+name = "4CH_vent"
 batch_size = 1
-num_epochs = 700 
+num_epochs = 500
 steps_per = 50
 num_folds = 0
 

cone_train.py
@@ -21,10 +21,15 @@ masks_path_2CH = path_2CH + "masks/"
 image_ds = np.load(numpy_path_2CH + "image_cone_ds.npy")
 masks_ds = np.load(numpy_path_2CH + "masks_cone_ds.npy")
 
+plt.imshow(image_ds[0,:,:,0])
+plt.show()
+plt.imshow(masks_ds[0,:,:,0])
+plt.show()
+
 #instantiate params
 name = "cone"
 batch_size = 1
-num_epochs = 100
+num_epochs = 130 
 steps_per = 50
 num_folds = 0
 

data.py
@@ -39,21 +39,52 @@ def pad_image(image, cx, cy, desired_size):
     exit(0)
   return crop
 
+def crop_image(image,cx,cy,desired_size):
+  X,Y = image.shape[0:2] 
+  r_x,r_y = int(desired_size[0] / 2),int(desired_size[1] / 2)
+  x1, x2 = cx - r_x, cx + r_x
+  y1, y2 = cy - r_y, cy + r_y
+  x1_, x2_ = max(x1, 0), min(x2, X)
+  y1_, y2_ = max(y1, 0), min(y2, Y)
+  # Crop the image
+  crop = image[x1_: x2_, y1_: y2_]
+  return crop
+
 def load_nii_image(image_path,frames,pad = True,dim = [800,800]):
 #loads nii to np and can zero pad to dim
 #returns padded nps from specific frames
-    image_nb = nib_load(image_path)
+    print("Analysing: " + image_path)
+    image_nb = nib.load(image_path)
     head = image_nb.header
     image_data = image_nb.get_fdata()
     [x,y,z,t] = image_data.shape
     cx,cy = int(x/2),int(y/2)
-    image_data_padded = pad_image(image_data,cx,cy,[dim[0],dim[1])
+    image_data_padded = pad_image(image_data,cx,cy,[dim[0],dim[1]])
+    image_frames = np.empty([0,dim[0],dim[1],1])
+    for i in range(len(frames)):
+        frame = image_data_padded[:,:,0,frames[i]-1]
+        print(frame.shape)
+        print(image_frames.shape)
+        frame = frame[np.newaxis,:,:,np.newaxis]
+        image_frames = np.append(image_frames,frame,axis = 0) 
 
-    return_dim = [len(frames),image_data_padded.shape[1],image_data_padded.shape[2]]
-    image_ds = np.empty_like(return_dim)
-    for i in range(return_dim[0])):
-        image_data_padded_frame[i,:,:,:] = image_data_padded[:,:,0,frames[i]-1]
-    return image_data_padded_frame,head
+    image_frames /= 255.0
+#only pad frames that we want doesnt work get mem error :(
+#    for i in range(len(frames)):
+#        image_data_frame = image_data[:,:,0,frames[i]-1]
+#        image_data_frame = image_data_frame[np.newaxis,:,:,np.newaxis]
+#        image_frames = np.append(image_frames,image_data_frame,axis = 0)
+#    print(image_frames.shape)
+#    [t,x,y,z] = image_frames.shape
+#    cx,cy = int(x/2),int(y/2)
+#    image_data_padded = pad_image(image_frames,cx,cy,[dim[0],dim[1]])
+#
+#    return_dim = [len(frames),dim[0],dim[1]]
+#    print(return_dim)
+#    print(image_data_padded.shape)
+#    image_ds = np.empty_like(return_dim)
+
+    return image_frames,head
 
 
 def load_nii_ds(dataset_path):
@@ -141,6 +172,10 @@ def load_nii_ds(dataset_path):
 #    image_ds.take(image_ds,image_cone_index,axis = 0,out = image_cone_ds)
     image_cone_ds = image_ds[image_cone_index,:,:,:]
     print(image_cone_ds.shape)
+    #remap from from 0-255 to 0-1
+    image_cone_ds /= 255.0
+    image_vent_ds /= 255.0
+
     return image_cone_ds,image_vent_ds,masks_cone_ds,masks_vent_ds,image_headers
 
 

data_generation.py
@@ -3,40 +3,77 @@ import sys
 import argparse
 import subprocess
 
+import numpy as np
 import pandas as pd
 import nibabel as nib
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 import data
 import params
 import unet
 
 path = "/home/zeke/Programming/cnn/us_seg/"
-data_gen = path + "data_gen/"
-
+load_path = "data/whole_dataset/"
+save_path = "data/data_gen/4CH_ds"
 #instantiate params
-name = "2CH_vent"
+dim = [800,800]
 
-2CH_data_generation_params = params.Params(dim = dim,
-                              experiment_name = name)
+data_generation_params_vent = params.Params(dim = dim,
+                              experiment_name = "2CH_vent")
+data_generation_params_cone = params.Params(dim = dim,
+                              experiment_name = "cone")
 #Load model and weights
-model = unet.Unet(2CH_data_generation_params)
-model.load_weights()
+#model_vent = unet.Unet(data_generation_params_vent)
+model_cone = unet.Unet(data_generation_params_cone)
 
-for filename in os.listdir(data_gen):
-    if filename.endswith(".nii.gz")
-        print(os.path.join(directory, filename))
-	#Open image with Iksnap
-	subprocess.call(["itksnap",filename])   
-	#Select Frames
-	num_frames = input("Enter number of frames: ")
-	frames = []
-	for i in range(num_frames):
-	    frames.append(input("Enter frame: "))
-	#nii to np
-	data.load_nii_image(filename,frames)
-	#np to nii
-
+for filename in os.listdir(load_path):
+    #Open image with Iksnap
+    subprocess.call(['itksnap',load_path + filename])   
+    #Select Frames
+    num_frames = int(input("Enter number of frames: "))
+    frames = []
+    for i in range(num_frames):
+        frames.append(int(input("Enter frame: ")))
+    #nii to np
+    image_frames,head = data.load_nii_image(load_path + filename,frames)
+    print(image_frames.shape)
+    plt.imshow(image_frames[0,:,:,0])
+    plt.show()
+    #predict cone masks
+    cone_masks = model_cone.make_prediction(image_frames)
+    print(cone_masks.shape)
+    plt.imshow(cone_masks[0,:,:,0])
+    plt.show()
+    #apply cone masks 
+    image_frames_nc = np.multiply(image_frames,cone_masks)
+    plt.imshow(image_frames_nc[0,:,:,0])
+    plt.show()
+    #predict vent masks 
+    vent_masks = model_vent.make_prediction(image_frames_nc)
+    plt.imshow(vent_masks[0,:,:,0])
+    plt.show()
+    #make masks OG resolution and back to nii
+    dim_padded = vent_masks.shape
+    dim_og =  head.get_data_shape()
+    cx,cy = int(dim_padded[1]/2),int(dim_padded[2]/2)
+    predicted_masks_og_dim = np.empty([vent_masks.shape[0],dim_og[0],dim_og[1],1])
+    for i in range(vent_masks.shape[0]):
+#        predicted_masks_og_dim[i,:,:,:] = data.crop_image(predicted_masks[i,:,:,:],cx,cy,dim_og)
+        mask = data.crop_image(vent_masks[i,:,:,:],cx,cy,dim_og)
+        print(mask.shape)
+        mask = mask[:,:,0]
+        mask = data.thresh_mask(mask,0.5)
+        plt.imshow(mask)
+        plt.show()
+        print(filename)
+        nii_ind = filename.find('.nii.gz')
+        maskname = filename[:nii_ind] + '_' + str(frames[i]) + filename[nii_ind:]
+        print(maskname)
+        nib.save(vent_masks[i,:,:,:],save_path + maskname)
+        #save to disk
+        #open in itksnap
+    print(predicted_masks_og_dim.shape)
+    #save to disk
     #Open Frames in ITK-snap with image for refinement
 
 

unet.py
@@ -26,12 +26,16 @@ class Unet():
 
     def get_Unet(self):
         model = Unet_arch(self.params.dim[0],self.params.dim[1])
-        model.summary()
+#        model.summary()
         model.compile(loss = bce_dice_loss,
                       optimizer = optimizers.Adam(),#self.params.optimizer
                       metrics = [dice_loss])
         return model
 
+    def load_weights(self,weights_path = None):
+        model = self.get_Unet()
+        model.load_weights(self.params.best)
+        return model
    
     def train(self,image_trn_ds,masks_trn_ds,image_val_ds = None,masks_val_ds = None,continue_train = False):
         #params
@@ -89,13 +93,13 @@ class Unet():
     def make_prediction(self,image):
         #takes image or images and generates predicted masks
         model = self.get_Unet()
-        print('defined model')
+        print('defined ' + self.params.experiment_name + ' model')
         #loads best weights
         model.load_weights(self.params.best)
         print('loaded best weights')
         prediction = np.empty_like(image)
         for i in range(image.shape[0]):
-            print("Segging Image: " + str(i))
+            print("Segging Image: " + str(i+1))
             #predictions = model.predict_generator(self.validation_gen)
             prediction[i,:,:,:] = model.predict(image[i:i+1,:,:,:])
         return prediction