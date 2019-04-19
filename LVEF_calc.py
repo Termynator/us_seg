434b022393d0461547cb532cb080e5a19136a2e9
Zeke <zekeortiz00@gmail.com>
2019-04-19 20:06:35 +0100

    report and val scripts

---
+++ LVEF_calc.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import unet
+import params
+
+# generate mask?
+# get 2CH and 4CH view
+# from masks annotate base of mitrial valve
+# simpsons biplane
+
---
+++ vent_validation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+import unet
+import params
+
+#load model
+#load coresponding 5 fold cv validation set
+#ROC
+#test 50? thresholds on every datapoint in val set
+
+# get avg bce dice loss
+
