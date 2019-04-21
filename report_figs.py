e76ab3b6481be977c638d26f91ccd50affb6283a
Zeke Ortiz <zekeortiz00@gmail.com>
2019-04-21 11:56:49 +0100

    added 5 fold stuff for rep figs

--- report_figs.py
+++ report_figs.py
@@ -6,22 +6,32 @@ path = "/home/zeke/Programming/cnn/us_seg/"
 data_path = path + "data/vent_dataset/"
 numpy_path = data_path + "numpys/"
 model_path = path + "models/"
+figure_path = path + "report/images/"
 
 # load numpys
-image_ds = np.load(numpy_path + "image_nc_ds.npy")
+image_cn_ds = np.load(numpy_path + "image_ds.npy")
+image_nc_ds = np.load(numpy_path + "image_nc_ds.npy")
 masks_ds = np.load(numpy_path + "masks_vent_ds.npy")
 
+# images for explanation
 image = image_ds[0,:,:,:]
 mask = masks_ds[0,:,:,:]
 
-experiment_name = "vent"
+# 5 fold cv training progress
+for i in range(0,5):
+    name = "vent_cv_" + str(i+1)
+    
+    training_df = pd.read_csv(model_path + name + ".csv")
 
-training_df = pd.read_csv(model_path + experiment_name + ".csv")
+    epochs = training_df.iloc[:,0]
+    trn_loss = training_df.iloc[:,1]
+    val_loss = training_df.iloc[:,4]
 
-epochs = training_df.iloc[:,0]
-loss = training_df.iloc[:,1]
-val_loss = training_df.iloc[:,4]
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Fold ' + str(i+1))
+    plt.legend()
 
+    plt.plot(epochs, trn_loss, val_loss,)
 
-plt.plot(epochs,loss,val_loss)
-plt.show()
+    plt.savefig('fold_' + str(i+1) + '.png')
