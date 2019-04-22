cf5ba89e62a51f42ad4043e426f1cb6a38b2658d
Zeke Ortiz <zekeortiz00@gmail.com>
2019-04-22 11:11:40 +0100

    report overhaul fixed bibtex

--- report/latex_compile.sh
+++ report/latex_compile.sh
@@ -3,12 +3,10 @@
 #compile latex into pdf with bib and display pdf
 
 name=( "${1%%.*}" .tex)
-pdflatex ${name}.tex
-pdflatex ${name}.tex
+latex ${name}.tex
 bibtex ${name}.tex 
-bibtex ${name}.tex 
-pdflatex ${name}.tex 
-pdflatex ${name}.tex
+latex ${name}.tex
+latex ${name}.tex
 pdflatex ${name}.tex
 xdg-open ${name}.pdf
 
